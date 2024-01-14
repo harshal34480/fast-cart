4e8ef7a998de942a8cacf17be6809b852b7d3fe1
harshal34480 <harshal34480@gmail.com>
2024-01-14 20:23:18 +0530

    style items

--- src/pages/homepage/HomePage.component.jsx
+++ src/pages/homepage/HomePage.component.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 // import "./HomePage.styles.scss";
 import Directory from '../../components/directory/directory.component'
 
-const HomePage = () => (
-  <div className="homePage">
-    <Directory />
-  </div>
-)
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <Directory />
+    </div>
+  )
+}
 
 export default HomePage
